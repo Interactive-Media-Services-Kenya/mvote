f09205ea954e9d131dab2fe3fd311975b5a51f49
tim <onyanchanyambane@gmail.com>
2026-02-02 13:35:13 +0300

    fix: random text on api route

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,6 @@ use App\Http\Controllers\STKPushController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/initiate-stk-push', [STKPushController::class, 'initiateSTK']);
+Route::get('testing', function () {
+    return 'Yay!';
+});
